214f63da945f1fde14689d5faa2c2bca09601150
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-10 22:19:30 +0500

    update

--- Question2.dart
+++ Question2.dart
@@ -1,17 +1,13 @@
 import 'dart:math';
 
 bool function(int number) {
-  int originalNumber = number;
-  int numDigits = number.toString().length;
   int sum = 0;
-
   while (number > 0) {
     int digit = number % 10;
-    sum += pow(digit, numDigits).toInt();
+    sum += pow(digit, number.toString().length).toInt();
     number ~/= 10;
   }
-
-  return sum == originalNumber;
+  return sum == number;
 }
 
 void main() {
--- Question5.dart
+++ Question5.dart
@@ -1,7 +1,8 @@
 String function({required int expense, required int price}) {
-  return ('You can booked your profit amount : ${price - expense}');
+  return ('You can booked your ${price - expense < 0 ? 'loss' : 'profit'} amount : ${(price - expense).abs()}');
 }
 
 void main() {
   print(function(expense: 500, price: 700));
+  print(function(expense: 1000, price: 700));
 }
