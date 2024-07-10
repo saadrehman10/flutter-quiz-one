409efd0bb58918f8de6eccc22a3c790918c771fa
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-10 22:02:21 +0500

    update

--- Question3.dart
+++ Question3.dart
@@ -1,1 +1,14 @@
-void main() {}
+List<int> function1(List<int> list) {
+  List<int> output = [];
+  for (int i = 1; i < list.length; i++) {
+    output.add((list[i] - list[i - 1]).abs());
+  }
+
+  return output;
+}
+
+void main() {
+  List<int> list = [1, 1, 3, 4, 4, 5, 6, 7];
+
+  print(function1(list));
+}
---
+++ Question4.dart
@@ -0,0 +1,13 @@
+void main() {
+  int row = 5;
+  for (int i = 0; i <= row - 1; i++) {
+    String temp = '';
+    for (int j = row - 1; j <= i; j--) {
+      temp += ' ';
+    }
+    for (int k = 0; k >= i; k++) {
+      temp += "* ";
+    }
+    print(temp);
+  }
+}
