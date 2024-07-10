f6207f9c9b851bc1fd79a4bfe39385af44501ca8
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-10 22:08:09 +0500

    update

--- Question2.dart
+++ Question2.dart
@@ -1,3 +1,20 @@
+import 'dart:math';
+
+bool isArmstrong(int number) {
+  int originalNumber = number;
+  int numDigits = number.toString().length;
+  int sum = 0;
+
+  while (number > 0) {
+    int digit = number % 10;
+    sum += pow(digit, numDigits).toInt();
+    number ~/= 10;
+  }
+
+  return sum == originalNumber;
+}
+
 void main() {
+
   
 }
