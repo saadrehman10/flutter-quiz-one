3dc38a318239d508864519862a938aa46e844763
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-10 22:06:12 +0500

    update

--- Question5.dart
+++ Question5.dart
@@ -1,3 +1,7 @@
+String function({required int expense, required int price}) {
+  return ('You can booked your profit amount : ${price - expense}');
+}
+
 void main() {
-  
+  print(function(expense: 500, price: 700));
 }
